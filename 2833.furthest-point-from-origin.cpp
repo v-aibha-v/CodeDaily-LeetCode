a2550536d74605489f56b0421ab4b2282caf58c3
v-aibha-v <vaibhavv08.05@gmail.com>
2026-04-24 07:19:16 +0530

    April 24,  most optimial,2833

--- 2833.furthest-point-from-origin.cpp
+++ 2833.furthest-point-from-origin.cpp
@@ -24,16 +24,17 @@ public:
         // rec(moves,0,0,moves.size());
         // return maxi;
         int n=moves.size();
-         vector<int> dpp(n+1,0);
-        vector<int> dp(n+1,0);
+        //  vector<int> dpp(n+1,0);
+        // vector<int> dp(n+1,0);
+        int p1=0,c1=0,p2=0,c2=0;
         for(int i=1;i<=n;i++){
-            if(moves[i-1]=='L') dp[i]=1+dp[i-1];
+            if(moves[i-1]=='L') c1=1+p1;
             // else if(moves[i-1]=='R') dp[i]=dp[i-1]-1;
             // else dp[i]=max(dp[i-1]-1,1+dp[i-1]);
-            else dp[i]=dp[i-1] -1;
-            if(moves[i-1]=='R')  dpp[i]=dpp[i-1] -1;
-            else dpp[i]=1+dpp[i-1];
-            
+            else c1=p1-1;
+            if(moves[i-1]=='R')  c2=p2 -1;
+            else c2=1+p2;
+            p2=c2;p1=c1;
         }
         //  vector<int> dpp(n+1,0);
         // for(int i=1;i<=n;i++){
@@ -42,7 +43,7 @@ public:
         //     // else dpp[i]=max(dpp[i-1]-1,1+dpp[i-1]);
         //     else dpp[i]=1+dpp[i-1];
         // }
-        return max(abs(dp[n]),abs(dpp[n]));
+        return max(abs(p1),abs(p2));
     }
 };
 // @lc code=end
